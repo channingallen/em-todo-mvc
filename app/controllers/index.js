b780da43f4f066f16a297bb0e90e9f6a5d260140
Channing Allen <channingallen@gmail.com>
2015-09-29 13:24:27 -0700

    add first component: status-checkbox

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,11 +4,6 @@ export default Ember.Controller.extend({
   actions: {
     removeTask(task) {
       task.deleteRecord();
-    },
-
-    toggleTaskStatus(task) {
-      var initialTaskStatus = task.get('completed');
-      task.set('completed', !initialTaskStatus);
     }
   }
 });
