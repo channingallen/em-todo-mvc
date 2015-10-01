36fc0e8160cd1fe9b01dc59ca35da2bf9c11d14b
Channing Allen <channingallen@gmail.com>
2015-10-01 12:50:01 -0700

    add focus-out option to editable task row component

--- app/components/editable-task-name.js
+++ app/components/editable-task-name.js
@@ -2,13 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editing: false,
+  
+  tagName: 'span',
 
   actions: {
     handleInputEnter(inputValue) {
       this.attrs.rename(inputValue);
       this.set('editing', false);
     },
-    
+
     startEditing() {
       this.set('editing', true);
     },
