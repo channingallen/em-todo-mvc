be12fcac25b98ae05401883bf0acd33ef0446563
Channing Allen <channingallen@gmail.com>
2015-10-01 13:00:39 -0700

    add strikethrough functionality to editable-task-name component

--- app/controllers/completed.js
+++ app/controllers/completed.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    renameTask(task, newName) {
+      task.set('name', newName);
+    },
+
+    removeTask(task) {
+      task.deleteRecord();
+    },
+
+    toggleTaskStatus(task, statusAfterToggle) {
+      task.set('completed', statusAfterToggle);
+    }
+  }
 });
