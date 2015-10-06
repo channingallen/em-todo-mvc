1d001cd3f1e5c98b9a3db0e3c0ed2ed9329833f2
Channing Allen <channingallen@gmail.com>
2015-10-05 17:54:40 -0700

    add new-task-input clear functionality via input value attribute

--- app/components/new-task-input.js
+++ app/components/new-task-input.js
@@ -3,15 +3,10 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'span',
 
-  inputViewName: 'whatever',
-
   actions: {
     submitNewTask(inputValue) {
       this.attrs.enter(inputValue);
-
-      const inputViewName = this.get('inputViewName');
-      const inputComponent = this.get(inputViewName);
-      inputComponent.set('value', null);
+      this.set('inputValue', '');
     }
   }
 });
